d0d59410e93cb5387b0400ee1df16b6266860475
Luis Alvarez D. <luis@vercel.com>
2026-05-27 12:54:26 -0500

    Updated delay

--- init.lua
+++ init.lua
@@ -397,7 +397,7 @@ do
   vim.pack.add { gh 'folke/which-key.nvim' }
   require('which-key').setup {
     -- Delay between pressing a key and opening which-key (milliseconds)
-    delay = 0,
+    delay = 200,
     icons = { mappings = vim.g.have_nerd_font },
     -- Document existing key chains
     spec = {
